b77cd29e79f1f8ff83362426ac5cde7071806e5d
Sebastian Perry <darklink.perry@gmail.com>
2022-03-01 02:36:48 -0500

    moved type enum to export

---
+++ poketype.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+# Pokemon types
+class Type(Enum):
+    NONE = 0
+    NORMAL = 1
+    ELECTRIC = 2
+    PSYCHIC = 3
+    POISON = 4
+    GHOST = 5
+    FIRE = 6
+    WATER = 7
+    GROUND = 8
+    FIGHTING = 9
+    GRASS = 10
+    FLYING = 11
+    BUG = 12
+    DRAGON = 13
+    FAIRY = 14
+    STEEL = 15
+    DARK = 16
+    ICE = 17
+    ROCK = 18
--- solve.py
+++ solve.py
@@ -5,27 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import sqlite3
 
-# Pokemon types
-class Type(Enum):
-    NONE = 0
-    NORMAL = 1
-    ELECTRIC = 2
-    PSYCHIC = 3
-    POISON = 4
-    GHOST = 5
-    FIRE = 6
-    WATER = 7
-    GROUND = 8
-    FIGHTING = 9
-    GRASS = 10
-    FLYING = 11
-    BUG = 12
-    DRAGON = 13
-    FAIRY = 14
-    STEEL = 15
-    DARK = 16
-    ICE = 17
-    ROCK = 18
+from poketype import Type
 
 # Possible clues
 class Clue(Enum):
